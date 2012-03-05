972987aec2c24a3e1636f2e6e33d849df5b1526e
Slawomir Zak <slawomir.zak@billin.pl>
2012-03-05 15:22:58 +0100

    added easy CRM example

---
+++ php5/examples/easy-crm/02-select-product.php
@@ -0,0 +1,37 @@
+<?
+require '../billin.php5';
+
+## PHP session start
+session_start();
+
+## Billin session start
+$billin = new BillinSession();
+$_SESSION['XBS-SID'] = $billin->sid;
+$billin->create_billing_data(array(fname => $_POST['fname'], 
+                                   lname => $_POST['lname'],
+                                   email => $_POST['email']));
+$customer = $billin->create_customer();
+$_SESSION['CUSTOMER_ID'] = $customer->id;
+$billin->commit();
+?>
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf8">
+        <title>Wybierz produkt</title>
+    </head>
+    <body>
+
+	<p>Wybierz produkt z listy:</p>
+	<form action='03-create-subscription.php' method="POST">
+		<select name="productid">
+		<?
+			foreach ($billin->list_products() as $prod) {
+				printf("\t\t<option value='%s'>%s</option>\n", $prod[id], $prod[name]);
+			}
+		?>
+		</select>
+		<p><input type="submit" value="Kup abonament"></p>
+	</form>
+    </body>
+</html>
---
+++ php5/examples/easy-crm/03-create-subscription.php
@@ -0,0 +1,39 @@
+<?
+require '../billin.php5';
+
+session_start();
+$billin = new BillinSession($_SESSION['XBS-SID']);
+
+$customer = $billin->find_customer(array(id => $_SESSION['CUSTOMER_ID']));
+$subscription = $billin->create_subscription(ref(-1));
+$product = $billin->get_product_params($_POST['productid'], ref(-2));
+$billin->assign_product($product, ref(-2));
+$result = $billin->commit();
+?>
+
+<?
+if ($result) {
+?>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf8">
+	</head>
+	<body>
+		<p>Właśnie zostałeś abonentem!</p>
+
+		<p>Oto faktura VAT za Twój zakup:</p>
+		<?
+		$billin->list_customer_invoices($customer);
+		$billin->elt(0);
+		$output = tmpfile();
+		$billin->get_document_image($output, ref(-1), keyword('html'), keyword('original'));
+		fseek($output, 0);
+		$image = stream_get_contents($output);
+		print($image);
+		?>
+		<p>Dziękujemy!</p>
+	</body>
+<html>
+<?
+}
+?>
