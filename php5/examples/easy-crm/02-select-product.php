37446e02ddb36536ca973737a43f987a8da50771
Slawomir Zak <slawomir.zak@billin.pl>
2012-03-08 13:16:36 +0100

    layout updates

--- php5/examples/easy-crm/02-select-product.php
+++ php5/examples/easy-crm/02-select-product.php
@@ -24,19 +24,24 @@ $billin->commit();
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf8">
         <title>Wybierz produkt</title>
+        <style>
+            body { font-family: Verdana, Arial, Sans-serif }
+            select { border: 1px solid balck; background: white; color: black }
+            input[type="submit"] { border: 0; background: #333; color: white; padding: 10px; margin-top: 20px }
+        </style>
     </head>
     <body>
 
-	<p>Wybierz produkt z listy:</p>
 	<form action='03-create-subscription.php' method="POST">
+        <label>Wybierz produkt z listy:
 		<select name="productid">
 		<?
 			foreach ($billin->list_products() as $prod) {
 				printf("\t\t<option value='%s'>%s</option>\n", $prod[id], $prod[name]);
 			}
 		?>
-		</select>
-		<p><input type="submit" value="Kup abonament"></p>
+		</select></label>
+		<p><input type="submit" value="Kup abonament >>"></p>
 	</form>
     </body>
 </html>
